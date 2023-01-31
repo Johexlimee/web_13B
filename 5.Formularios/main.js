3b5c405463fcddefde0b67276433a9ba5c2fc6ec
Camilo Rincon <crincon@uniempresarial.edu.co>
2023-01-31 12:27:00 -0500

    Primera Version

---
+++ 5.Formularios/main.js
@@ -0,0 +1,12 @@
+'use strict';
+//Variables o contenedores
+var numero1 = 13;
+let numero2 = 25;
+const IVA = 19.1;
+
+if (true) {
+    let numero2 = 50;
+    console.log(numero2);
+}
+
+console.log(numero2);
