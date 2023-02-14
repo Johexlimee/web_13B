834db8b2e0db73be3254ee054260c480d3ffa399
Camilo Rincon <crincon@uniempresarial.edu.co>
2023-02-14 12:33:01 -0500

    Feb-14

--- PracticaGuiada/scripts/main.js
+++ PracticaGuiada/scripts/main.js
@@ -1,5 +1,5 @@
 import {validateString, vAvg, checkAvg} from './validate.js';
-import {paintCard, addStudent} from './paint.js'
+import {paintCard, addStudent,modalAlert} from './paint.js'
 
 const btnAgregar = document.getElementById('btnAgregar');
 const btnMostrar = document.getElementById('btnMostrar');
@@ -23,12 +23,22 @@ const btnMostrar = document.getElementById('btnMostrar');
 btnAgregar.onclick = function () {
     const name = document.getElementById('nombre').value;
     const lastName = document.getElementById('apellido').value;
-    const avg = document.getElementById('promedio').value;
+    const avg = parseFloat(document.getElementById('promedio').value);
     const op = document.getElementById('opcion').value;
 
     // console.log(`${name} ${lastName} ${avg} ${op}`);
 
-    if (validateString(name) && validateString(lastName) && opcion !=0){
-        
+    if (validateString(name) && validateString(lastName) && op !=0){
+        if ((!isNaN(avg)) && (vAvg(avg))){
+            addStudent(name,lastName,avg)
+        } else {
+            document.querySelector('#promedio').value="";
+            modalAlert("Promedio Invalido");
+        }
+    } else {
+        modalAlert("Datos invalidos, revisar campos");
     }
 }
+btnMostrar.addEventListener('click', function(){
+    paintCard("ESTUDIANTE");
+})
--- PracticaGuiada/scripts/paint.js
+++ PracticaGuiada/scripts/paint.js
@@ -1,4 +1,3 @@
-'use strict';
 import {checkAvg} from './validate.js'
 const cardE = document.getElementById('cardsEstudiantes');
 const students = [];
@@ -6,15 +5,15 @@ const students = [];
 const paintCard = (typ) => {
     typ = typ.toUpperCase();
     const fragment = document.createDocumentFragment();
-    const templateStudent = document.querySelector('#templateEstudiantes').content;
+    const templateStudent = document.querySelector('#templateEstudiante').content;
 
     if (typ === 'ESTUDIANTE') {
         for (let i of students){
             const cloneTemp = templateStudent.cloneNode(true);
             cloneTemp.querySelector('.title-card').innerHTML = "Datos del <i>Estudiante</i>.";
-            cloneTemp.querySelector('.data-card').innerHTML = `NOMBRE: ${i.nombre.toUpperCase()} APELLIDOS: ${i.apellido.toUpperCase()}`;
-            cloneTemp.querySelector('.text-promedio').innerHTML = `PROMEDIO ES: ${i.promedio}`;
-            cloneTemp.querySelector(`.text-aprobado`).innerHTML = `${checkAvg(i.promedio)}`;
+            cloneTemp.querySelector('.data-card').innerHTML = `NOMBRE: ${i.nom.toUpperCase()} APELLIDOS: ${i.ape.toUpperCase()}`;
+            cloneTemp.querySelector('.text-promedio').innerHTML = `PROMEDIO ES: ${i.prom}`;
+            cloneTemp.querySelector(`.text-aprobado`).innerHTML = `${checkAvg(i.prom)}`;
             fragment.appendChild(cloneTemp);
         }
     } else {
@@ -30,7 +29,29 @@ const addStudent = (name,lastname,avg) => {
         ape: lastname,
         prom: avg
     }
-    student.push(student);
+    students.push(student);
     alert('Se agrego estudiante');
 }
-export {paintCard,addStudent}
+
+const modalAlert = (cad) => {
+    const alerta = document.createElement('div');
+    const texto = document.createElement('p');
+    const img = document.createElement('img');
+    img.src = './img/cruz.png';
+    img.className="close";
+    texto.setAttribute("class","textAlerta");
+    alerta.setAttribute("id","alerta");
+    alerta.setAttribute("class","alerta");
+    texto.innerHTML = `<strong>${cad}</strong>`;
+    alerta.appendChild(img);
+    alerta.appendChild(texto);
+    document.body.appendChild(alerta);
+    img.onclick = function(){
+        document.getElementById("alerta").remove();
+    }
+
+}
+
+export {paintCard,addStudent,modalAlert}
+
+
