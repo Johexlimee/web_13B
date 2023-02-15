3178aab460f04dce06d3c17786b33642733d7c7f
Camilo Rincon <crincon@uniempresarial.edu.co>
2023-02-15 15:18:37 -0500

    Feb-15

---
+++ ProyectoTienda/dist/app.js
@@ -0,0 +1,10 @@
+'use strict';
+//Efecto buscador
+const buscando = document.querySelector('#buscando');
+const inputBuscar = document.querySelector('#input_buscar');
+
+buscando.addEventListener('click', (e) => {
+    if (inputBuscar.classList.contains('buscarOculto')) {
+
+    }
+});
